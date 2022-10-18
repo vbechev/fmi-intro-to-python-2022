c8904e4696579e1a7485781efdf935aa21ed6601
vbechev <victor.bechev@gmail.com>
2022-10-18 17:51:10 +0300

    Add a sample test for homework 01

---
+++ 01/sample_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from solution import calculate_final_vector
+
+
+class TestCalculateFinalVector(unittest.TestCase):
+    '''Test the calculate_final_vector function.'''
+
+    def test_simple_move(self):
+        '''Sanity test for a simple 2-step move.'''
+        self.assertEqual(calculate_final_vector((0, 0), ['00c000', 'c0c000']),
+                         (1, 1))
+
+if __name__ == '__main__':
+    unittest.main()
+
