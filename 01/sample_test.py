c31e14c5c0dabbd7f95525cfd5c6c2d0c61dc944
ybabulkov <ibabulkov@gmail.com>
2022-10-23 18:23:35 +0300

    Suggest a correction following PEP 257

--- 01/sample_test.py
+++ 01/sample_test.py
@@ -4,10 +4,10 @@ from solution import calculate_final_vector
 
 
 class TestCalculateFinalVector(unittest.TestCase):
-    '''Test the calculate_final_vector function.'''
+    """Test the calculate_final_vector function."""
 
     def test_simple_move(self):
-        '''Sanity test for a simple 2-step move.'''
+        """Sanity test for a simple 2-step move."""
         self.assertEqual(calculate_final_vector((0, 0), ['00c000', 'c0c000']),
                          (1, 1))
 
